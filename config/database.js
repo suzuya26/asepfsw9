ac3be48651396b691f04190af71ea26f19a7724b
Farhan Septiadi <55498270+suzuya26@users.noreply.github.com>
2022-06-07 20:12:59 +0700

    nambah konekan ke gitab

--- config/database.js
+++ config/database.js
@@ -1,34 +1,52 @@
 const {
-  DB_USER = "",
-  DB_PASSWORD = "",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
+  DB_USER = '',
+  DB_PASSWORD = '',
+  DB_NAME = 'bcr',
+  DB_HOST = '127.0.0.1',
+  DB_PORT = '5432',
 } = process.env;
-
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
-  }
-}
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+
+};
